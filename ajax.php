f64bb1e91ea2d12b9aaeb0fa73cbbf9f3f825b62
Nicolai Søborg <NicolaiSoeborg+github@gmail.com>
2016-07-30 01:13:03 +0200

    Fixes

--- ajax.php
+++ ajax.php
@@ -4,8 +4,9 @@ error_reporting(E_ALL);*/
 
 header('Content-Type: application/json');
 
-$result = apcu_fetch("KBOpen");
-if ($result === false) {
+$result = "Unknown";
+/*$result = apcu_fetch("KBOpen");
+if ($result === false) {*/
     //echo "console.log(\"Tjekker om KB er åben...\");";
     $xml = file_get_contents("http://xn--erklderbarenben-slbh.dk/");
     $a = strpos($xml, "<div id=\"svar\">");
@@ -13,7 +14,7 @@ if ($result === false) {
         $b = strpos($xml, "</div>", $a);
         if ($b !== FALSE) {
             $nope = array("False","Nope", "Nej","Beklager","Niksen Biksen","Niks", "No");
-			$yeah = array("True", "Yeah", "Ja", "Yeah", "Tjek",   "Jepper","Jep",  "Yes");
+            $yeah = array("True", "Yeah", "Ja", "Yeah", "Tjek",   "Jepper","Jep",  "Yes");
 
             $svar = substr($xml, $a+15, $b-($a+15));
 
@@ -21,11 +22,11 @@ if ($result === false) {
             if (in_array($svar, $yeah)) $result = "Yes";
             if (in_array($svar, $nope)) $result = "No";
 
-            apcu_store("KBOpen", $result, 15*60); // Cache in 15 min
+            //apcu_store("KBOpen", $result, 15*60); // Cache in 15 min
 
-        } else apcu_store("Error", true, 60);
-    } else apcu_store("Error", true, 60);
-} else $result = "No";
+        } //else apcu_store("Error", true, 60);
+    } //else apcu_store("Error", true, 60);
+//} else $result = "No";
 
 echo $result;
 ?>
